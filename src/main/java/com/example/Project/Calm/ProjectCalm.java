1892d04b93b5d5c51dd1e56e050e2221fe1bdfd1
rocky2560 <sudipmanandhar2560@gmail.com>
2025-12-06 21:21:54 +0545

    second commit

---
+++ src/main/java/com/example/Project/Calm/ProjectCalm.java
@@ -0,0 +1,13 @@
+package com.example.Project.Calm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalm {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCalm.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project/Calm/ProjectCalmTests.java
@@ -0,0 +1,13 @@
+package com.example.Project.Calm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCalmTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
